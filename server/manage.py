ebcc5550a15d1d73f47dca97aca526898d010278
Andres Camperos <andrescamperoscram1005@gmail.com>
2023-11-13 23:50:41 -0500

    feat: change name to backend folder

--- server/manage.py
+++ server/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calzadoan_backend.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storage_backend.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
